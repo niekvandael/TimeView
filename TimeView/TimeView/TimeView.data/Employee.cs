9a8dd5105edb1461c1c1b2331c4943cf8be3fa27
niekvandael <nvd@asist.be>
2016-01-13 15:10:40 +0100

    Project setup + initial entities

---
+++ TimeView/TimeView/TimeView.data/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeView.data
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+
+        public List<Schedule> Schedules { get; set; }
+    }
+}
---
+++ TimeView/TimeView/TimeView.data/Schedule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TimeView.data
+{
+    public class Schedule
+    {
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+    }
+}
